43b51fa644ab85d78af474caea214b611a84c4e0
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-07 08:20:38 +0100

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        latoRegular: 'Lato-Regular',
-        latoBold: 'Lato-Bold',
+        latoRegular: "Lato-Regular",
+        latoBold: "Lato-Bold",
       },
     },
   },
-  //   plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 };
